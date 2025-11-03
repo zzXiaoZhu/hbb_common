2842db9e9122b0f39048e821d07db1d58fe6b2ef
zzXiaoZhu <xiaozhu13464298970@outlook.com>
2025-11-03 13:06:08 +0800

    修改默认服务器为小猪服务器

--- src/config.rs
+++ src/config.rs
@@ -105,8 +105,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.106.72.237"];
+pub const RS_PUB_KEY: &str = "ysdZhHxsKj5MOEb10HQjAxftj6L1stUWsY0yZqKpoFc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
